c11f00d7b9fdeff80f01b299f761bf99351c7f40
Abel Campos <vr506simulacion@gmail.com>
2026-07-31 15:12:37 -0600

    fix(design-system): associate input errors via aria-describedby

--- resources/views/components/ui/input.blade.php
+++ resources/views/components/ui/input.blade.php
@@ -1,6 +1,7 @@
 @props(['name', 'label' => null, 'type' => 'text', 'error' => null])
 @php
     $id = $attributes->get('id', $name);
+    $errorId = $id . '-error';
 @endphp
 <div class="flex flex-col gap-1">
     @if ($label)
@@ -11,6 +12,7 @@
         id="{{ $id }}"
         name="{{ $name }}"
         type="{{ $type }}"
+        @if ($error) aria-invalid="true" aria-describedby="{{ $errorId }}" @endif
         {{ $attributes->except(['id', 'name', 'type'])->merge([
             'class' => 'min-h-[44px] rounded-sm border bg-surface px-3 font-sans text-base text-text '
                 . 'focus-visible:outline-none focus-visible:shadow-focus '
@@ -19,6 +21,6 @@
     />
 
     @if ($error)
-        <p class="font-sans text-sm text-danger">{{ $error }}</p>
+        <p id="{{ $errorId }}" class="font-sans text-sm text-danger">{{ $error }}</p>
     @endif
 </div>
