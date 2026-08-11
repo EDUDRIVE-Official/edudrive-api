c632a08e0ab706cfde3a7ac68140ac56783a432a
Abel Campos <vr506simulacion@gmail.com>
2026-08-10 20:48:17 -0600

    feat(academic): register question handlers in provider

--- modules/Academic/Infrastructure/Providers/AcademicServiceProvider.php
+++ modules/Academic/Infrastructure/Providers/AcademicServiceProvider.php
@@ -14,6 +14,8 @@ use Modules\Academic\Application\Commands\ChangeProgramAudienceCommand;
 use Modules\Academic\Application\Commands\CreateCompetencyCommand;
 use Modules\Academic\Application\Commands\CreateCourseCommand;
 use Modules\Academic\Application\Commands\CreateProgramCommand;
+use Modules\Academic\Application\Commands\CreateQuestionCommand;
+use Modules\Academic\Application\Commands\DeleteQuestionCommand;
 use Modules\Academic\Application\Commands\PublishCourseCommand;
 use Modules\Academic\Application\Commands\PublishProgramCommand;
 use Modules\Academic\Application\Commands\ReopenCourseCommand;
@@ -22,13 +24,16 @@ use Modules\Academic\Application\Commands\ReplaceProgramCoursesCommand;
 use Modules\Academic\Application\Commands\ReplaceUnitContentCommand;
 use Modules\Academic\Application\Commands\SendCourseBackToDraftCommand;
 use Modules\Academic\Application\Commands\SubmitCourseForReviewCommand;
+use Modules\Academic\Application\Commands\UpdateQuestionCommand;
 use Modules\Academic\Application\Queries\GetCourseCurriculumQuery;
 use Modules\Academic\Application\Queries\GetCourseVersionQuery;
+use Modules\Academic\Application\Queries\GetQuestionQuery;
 use Modules\Academic\Application\Queries\GetUnitContentQuery;
 use Modules\Academic\Application\Queries\ListCompetenciesQuery;
 use Modules\Academic\Application\Queries\ListCoursesQuery;
 use Modules\Academic\Application\Queries\ListCourseVersionsQuery;
 use Modules\Academic\Application\Queries\ListProgramsQuery;
+use Modules\Academic\Application\Queries\ListQuestionsQuery;
 use Modules\Academic\Application\UseCases\AddCompetencyIndicatorHandler;
 use Modules\Academic\Application\UseCases\AddSubcompetencyHandler;
 use Modules\Academic\Application\UseCases\ApproveCourseHandler;
@@ -38,13 +43,17 @@ use Modules\Academic\Application\UseCases\ChangeProgramAudienceHandler;
 use Modules\Academic\Application\UseCases\CreateCompetencyHandler;
 use Modules\Academic\Application\UseCases\CreateCourseHandler;
 use Modules\Academic\Application\UseCases\CreateProgramHandler;
+use Modules\Academic\Application\UseCases\CreateQuestionHandler;
+use Modules\Academic\Application\UseCases\DeleteQuestionHandler;
 use Modules\Academic\Application\UseCases\GetCourseCurriculumHandler;
 use Modules\Academic\Application\UseCases\GetCourseVersionHandler;
+use Modules\Academic\Application\UseCases\GetQuestionHandler;
 use Modules\Academic\Application\UseCases\GetUnitContentHandler;
 use Modules\Academic\Application\UseCases\ListCompetenciesHandler;
 use Modules\Academic\Application\UseCases\ListCoursesHandler;
 use Modules\Academic\Application\UseCases\ListCourseVersionsHandler;
 use Modules\Academic\Application\UseCases\ListProgramsHandler;
+use Modules\Academic\Application\UseCases\ListQuestionsHandler;
 use Modules\Academic\Application\UseCases\PublishCourseHandler;
 use Modules\Academic\Application\UseCases\PublishProgramHandler;
 use Modules\Academic\Application\UseCases\ReopenCourseHandler;
@@ -53,15 +62,18 @@ use Modules\Academic\Application\UseCases\ReplaceProgramCoursesHandler;
 use Modules\Academic\Application\UseCases\ReplaceUnitContentHandler;
 use Modules\Academic\Application\UseCases\SendCourseBackToDraftHandler;
 use Modules\Academic\Application\UseCases\SubmitCourseForReviewHandler;
+use Modules\Academic\Application\UseCases\UpdateQuestionHandler;
 use Modules\Academic\Domain\Repositories\CompetencyRepository;
 use Modules\Academic\Domain\Repositories\CourseRepository;
 use Modules\Academic\Domain\Repositories\CourseVersionRepository;
 use Modules\Academic\Domain\Repositories\ProgramRepository;
+use Modules\Academic\Domain\Repositories\QuestionRepository;
 use Modules\Academic\Domain\Repositories\UnitContentRepository;
 use Modules\Academic\Infrastructure\Persistence\Eloquent\Repositories\EloquentCompetencyRepository;
 use Modules\Academic\Infrastructure\Persistence\Eloquent\Repositories\EloquentCourseRepository;
 use Modules\Academic\Infrastructure\Persistence\Eloquent\Repositories\EloquentCourseVersionRepository;
 use Modules\Academic\Infrastructure\Persistence\Eloquent\Repositories\EloquentProgramRepository;
+use Modules\Academic\Infrastructure\Persistence\Eloquent\Repositories\EloquentQuestionRepository;
 use Modules\Academic\Infrastructure\Persistence\Eloquent\Repositories\EloquentUnitContentRepository;
 use Modules\Foundation\Application\Bus\MessageHandlerRegistry;
 
@@ -90,6 +102,11 @@ final class AcademicServiceProvider extends ServiceProvider
         );
 
         $this->app->bind(UnitContentRepository::class, EloquentUnitContentRepository::class);
+
+        $this->app->bind(
+            QuestionRepository::class,
+            EloquentQuestionRepository::class,
+        );
     }
 
     public function boot(
@@ -159,6 +176,12 @@ final class AcademicServiceProvider extends ServiceProvider
             ListCoursesQuery::class,
             ListCoursesHandler::class,
         );
+
+        $registry->register(CreateQuestionCommand::class, CreateQuestionHandler::class);
+        $registry->register(UpdateQuestionCommand::class, UpdateQuestionHandler::class);
+        $registry->register(DeleteQuestionCommand::class, DeleteQuestionHandler::class);
+        $registry->register(GetQuestionQuery::class, GetQuestionHandler::class);
+        $registry->register(ListQuestionsQuery::class, ListQuestionsHandler::class);
 
         $this->loadRoutesFrom(
             dirname(__DIR__, 2).'/Presentation/Routes/api.php',
