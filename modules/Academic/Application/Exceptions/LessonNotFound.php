451ccb7170bb549c9b29c43efd1bb562488f04ec
Abel Campos <vr506simulacion@gmail.com>
2026-08-15 19:15:20 -0600

    feat(academic): add lesson not found exception

---
+++ modules/Academic/Application/Exceptions/LessonNotFound.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\Academic\Application\Exceptions;
+
+use Modules\Foundation\Domain\Exceptions\DomainException;
+
+final class LessonNotFound extends DomainException
+{
+    public static function withId(string $id): self
+    {
+        return new self(
+            message: sprintf('No existe una leccion con el identificador %s en el curso de la inscripcion.', $id),
+            errorCode: 'LESSON_NOT_FOUND',
+            statusCode: 404,
+        );
+    }
+}
